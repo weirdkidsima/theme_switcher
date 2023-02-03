467753d52d4b6e995e223c396ecd9378303aca1c
weirdkidsima <61547155+weirdkidsima@users.noreply.github.com>
2023-02-03 17:41:58 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function changeTheme(event) {
+    const isDark = event.target.getAttribute("dark-mode") === "true";
+  
+    document.documentElement.classList.add("color-theme-in-transition");
+    window.setTimeout(function () {
+      document.documentElement.classList.remove("color-theme-in-transition");
+    }, 1000);
+  
+    if (isDark) {
+      document.documentElement.setAttribute("data-theme", "light");
+      event.target.setAttribute("dark-mode", "false");
+    } else {
+      event.target.setAttribute("dark-mode", "true");
+      document.documentElement.setAttribute("data-theme", "dark");
+    }
+  }
